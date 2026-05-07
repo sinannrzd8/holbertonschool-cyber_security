651f4d3125ec4f9a4f821c2880400be598496148
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-07 18:17:56 +0400

    Task1

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-CONFIG="/etc/ssh/sshd_config"
-
-if [ ! -f "$CONFIG" ]; then
-    echo "Config file not found"
-    exit 1
-fi
-
-# main config
-grep -Ev '^\s*#|^\s*$' "$CONFIG"
-
-# included configs
-if grep -q "^Include" "$CONFIG"; then
-    grep -Ev '^\s*#|^\s*$' /etc/ssh/sshd_config.d/*.conf 2>/dev/null
-fi
+grep -hEv '^\s*#|^\s*$' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf 2>/dev/null
