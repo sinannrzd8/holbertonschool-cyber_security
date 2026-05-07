1ad90974105dd418ff565d34fc7176e6ac8a5b64
sinannrzd8 <sinannrzd.2008@gmail.com>
2026-05-07 18:15:07 +0400

    Task 1

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-grep -E '^(Include|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive)' /etc/ssh/sshd_config
+
+CONFIG="/etc/ssh/sshd_config"
+
+if [ ! -f "$CONFIG" ]; then
+    echo "Config file not found"
+    exit 1
+fi
+
+# main config
+grep -Ev '^\s*#|^\s*$' "$CONFIG"
+
+# included configs
+if grep -q "^Include" "$CONFIG"; then
+    grep -Ev '^\s*#|^\s*$' /etc/ssh/sshd_config.d/*.conf 2>/dev/null
+fi
